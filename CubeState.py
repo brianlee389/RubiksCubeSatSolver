3ecc85d61e5e0968b8f14b787affab592a763cd6
Brian Lee <bl389@drexel.edu>
2015-02-14 12:15:06 -0500

    First

---
+++ CubeState.py
@@ -0,0 +1,52 @@
+from random import shuffle
+
+NUMFACE = 6
+NUMCOLORS = 6
+NUMFACELETS = 4
+
+def createCubeState(face,facelet,state,color=1):
+    return (face, facelet,state,color)
+
+# Assume (i,j,m,k)
+def colorFunctionCubeState(c):
+    f = 'c('+ str(c[0]) + ',' + str(c[1]) +',' + str(c[2]) +')'
+
+# Assume (i,j,m,k)
+
+#2x2x2 Cube Class
+# color order list contains number between 0-25
+class RubiksCube:
+    # generates for every state for every 
+    # face/facelet and assign colors based on colors
+    def __init__(self,colors):
+        self.states = self.createStates(colors)
+
+    def createStates(self, colors):
+        a = []
+        if len(colors) != 24:
+            print "incorrect colors"
+#            return []
+        # face number
+        for x in range(1,7):
+            # facelet
+            for y in range(0,4):
+                index = x+y
+                c = colors[index] % NUMFACE
+                b = createCubeState(x,y,1,c)
+                print b
+                a.append(b)
+        return a
+#        self.states = a
+#    def getStates(self):
+#        hi = self.a
+#        return hi
+
+colornums = range(1,25)
+pcolornums = range(1,25)
+shuffle(pcolornums)
+
+sample = RubiksCube(pcolornums)
+print sample
+#ss = sample.getStates
+for i in range(0,1):
+    print colorFunctionCubeState(sample.states[i])
---
+++ RubiksCubeSolver.py
@@ -0,0 +1,8 @@
+# 2 x 2 Rubiks Cube Solver using Minisat
+# This converts how to solve the Rubiks Cube into a Satisfiability Problem
+# Minisat will solve the steps needed to solve the cube.
+
+
+NUMFACE = 6
+NUMCOLORS = 6
+NUMFACELETS = 4
