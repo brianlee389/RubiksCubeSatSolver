a01780934e297c6767dfa7d0e71f90a05f6ec19f
Brian Lee <bl389@drexel.edu>
2015-02-15 22:47:51 -0500

    Added running minisat, renamed some code

--- CubeState.py
+++ CubeState.py
@@ -13,21 +13,21 @@ UP = 5
 DOWN = 6
 
 # Assume (i,j,m,k)
-def createCubeState(face,facelet,state,color=1):
-    return (face,facelet,state,color)
+def createCubeFacelet(face,facelet,color=1):
+    return (face,facelet,color)
 
 # Assume (i,j,m,k)
-def colorFunctionCubeState(c):
-    f = 'c('+ str(c[0]) + ',' + str(c[1]) +',' + str(c[2]) +')'
+def colorFunctionCubeState(c, state):
+    f = 'c('+ str(c[0]) + ',' + str(c[1]) +',' + str(state) +')'
     return f
 
 # Assume (i,j,m,k)
 def getColor(c):
-    return c[3]
+    return c[2]
 
 # face transition function for UP
 # clockwise - 90
-def uf(i):
+def upf(i):
     if i == 0:
         return 1
     elif i == 1:
@@ -37,11 +37,11 @@ def uf(i):
     elif i == 3:
         return 2
 
-def ug(i):
+def upg(i):
     return (i%4)+1
 
 # counter clockwise -90
-def ufprime(i):
+def upfprime(i):
     if i == 0:
         return 2
     elif i == 1:
@@ -51,11 +51,11 @@ def ufprime(i):
     elif i == 3:
         return 1
 
-def ugprime(i):
+def upgprime(i):
     return ((i+2) % 4) + 1
 
 # half turn 180
-def uf2(i):
+def upf2(i):
     if i == 0:
         return 3
     elif i == 1:
@@ -65,7 +65,7 @@ def uf2(i):
     elif i == 3:
         return 0
 
-def g2(i):
+def upg2(i):
     return ((i+1) % 4) + 1
 # end face transition functions
 
@@ -78,33 +78,33 @@ class RubiksCube:
         state =  self.createState(colors)
         self.States = []
         self.States.append(state)
-#       print self.States
+
     def createState(self, colors):
         a = []
         if len(colors) != 24:
             print "incorrect colors"
             return []
-        # face number
+        # generating face numbers
         for x in range(1,7):
-            # facelet
+            # generating facelet numbers
             for y in range(0,4):
-                index = x+y
+                index = 4*(x-1)+y
                 c = colors[index] % NUMFACE
-                b = createCubeState(x,y,STARTSTATE,c)
+                b = createCubeFacelet(x,y,c)
                 a.append(b)
         return a
     
     # incompleted
     def moveFunction(self, face, dir):
         sn = len(self.States)
-        fn = uf
-        gn = ug
+        fn = upf
+        gn = upg
         if dir == 1:
-            fn = ufprime
-            gn = ugprime
+            fn = upfprime
+            gn = upgprime
         elif dir == 2:
-            fn = uf2
-            gn = ug2
+            fn = upf2
+            gn = upg2
 
         #compute function
         # (fn,gn)
@@ -119,13 +119,14 @@ class RubiksCube:
         self.States.append(state)
 
 
+
+# test code
 colornums = range(1,25)
 pcolornums = range(1,25)
 shuffle(pcolornums)
 sample = RubiksCube(colornums)
 states = sample.getStates()
-print sample.hi()
-#print getColor(states)
-print states[0][0]
+
+print states[0]
 for i in range(0,1):
-    print colorFunctionCubeState(sample.States[0][0])
+    print colorFunctionCubeState(sample.States[i][0],i)
--- RubiksCubeSolver.py
+++ RubiksCubeSolver.py
@@ -1,8 +1,49 @@
 # 2 x 2 Rubiks Cube Solver using Minisat
 # This converts how to solve the Rubiks Cube into a Satisfiability Problem
 # Minisat will solve the steps needed to solve the cube.
-
-
+import os
+import subprocess
+STARTSTATE = 0
 NUMFACE = 6
 NUMCOLORS = 6
 NUMFACELETS = 4
+RED = [0, 1, 1]
+FRONT = 1
+BACK = 2
+LEFT = 3
+RIGHT = 4
+UP = 5
+DOWN = 6
+
+#
+class MinisatRubiks:
+	def __init__(self):
+		self.variables = 8
+		self.clauses = [[1, 2, 3,4,5,6,7,8],[1],[-2]]
+		self.variablemapping = []
+
+	def createSatFile(self):
+		numclauses = len(self.clauses)
+		f = open('satrubiks.in', 'w')
+		f.write('p cnf ')
+		f.write(str(self.variables) + ' ')
+		f.write(str(numclauses) + '\n')
+		for cl in self.clauses:
+			for v in cl:
+				f.write(str(v) + ' ')
+			f.write('0\n')
+
+	def callMinisat(self):
+		subprocess.call(["minisat", "satrubiks.in", "satrubiks.out"])
+
+	def parseMinisat(self):
+		f = open('satrubiks.out', 'r')
+		hi = f.readline()
+		print "Variables"
+		raw = f.read()
+		vars = str.split(raw,' ')
+		print vars
+h = MinisatRubiks()
+h.createSatFile()
+h.callMinisat()
+h.parseMinisat()
