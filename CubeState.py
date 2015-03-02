2c62b520e26164d9cb645b1d2b9a51b5b9a89576
Brian Lee <bl389@drexel.edu>
2015-03-01 23:56:06 -0500

    Move functions- added function that does all the moves for a state

--- CubeState.py
+++ CubeState.py
@@ -23,9 +23,9 @@ upIndex = (UP-1)*4
 downIndex = (DOWN-1)*4
 
 frontFacelets = range(frontIndex, frontIndex+4)
+leftFacelets = range(leftIndex, leftIndex+4)
 backFacelets = range(backIndex, backIndex+4)
 rightFacelets = range(rightIndex, rightIndex+4)
-leftFacelets = range(leftIndex, leftIndex+4)
 upFacelets = range(upIndex, upIndex+4)
 downFacelets = range(downIndex, downIndex+4)
         
@@ -55,23 +55,24 @@ def getFacelet(c):
 def getFace(c):
     return c[0]
 
-def oppositeFace(face):
-    facen = face+1
-    if facen == FRONT:
-        return BACK
-    elif facen == BACK:
-        return FRONT
-    elif facen == LEFT:
-        return RIGHT
-    elif facen == RIGHT:
-        return LEFT
-    elif facen == UP:
-        return DOWN
-    elif facen == DOWN:
-        return UP
-    else:
-        print "Doesn't work"
-        return -1
+# def oppositeFace(face):
+#     facen = face+1
+#     if facen == FRONT:
+#         return BACK
+#     elif facen == BACK:
+#         return FRONT
+#     elif facen == LEFT:
+#         return RIGHT
+#     elif facen == RIGHT:
+#         return LEFT
+#     elif facen == UP:
+#         return DOWN
+#     elif facen == DOWN:
+#         return UP
+#     else:
+#         print "Doesn't work"
+#         return -1
+
 #2x2x2 Cube Class
 # color order list contains number between 0-25
 class RubiksCube:
@@ -103,6 +104,7 @@ class RubiksCube:
         return a
 
     # could be abstracted but did it this way to easily understand
+    # each move function needs a direction as an input
     def upMove(self, dir):
         moveface = upG
         movefacelet = cw
@@ -288,13 +290,15 @@ class RubiksCube:
     def leftMove(self, dir):
         moveface = leftG
         movefacelet = cw
+        sidefacelet = cwleft
         if dir == CCW:
             moveface = leftGprime
             movefacelet = ccw
+            sidefacelet = ccwleft
         elif dir == HALF:
             moveface = leftG2
             movefacelet = half
-
+            sidefacelet = halfleft
         nextStateNum = self.CurrentStateNum + 1
         # create new deep copies of states
         oldState = copy.deepcopy(self.States[self.CurrentStateNum])
@@ -317,28 +321,71 @@ class RubiksCube:
             print pclause
         print "-----"
         
-        # changed
-        for j in rangelist:
+        #changed = []
+        # front (0,1) -> change for 0,2
+        rangefl = frontFacelets + downFacelets + upFacelets
+        sidefirst = 0
+        sidesnd = 1
+        print "length" + str(len(rangefl))
+        for j in rangefl:
             # facelet state
             facelet = oldState[j]
+            flnum = getFacelet(facelet)
+            facen = getFace(facelet)
+            
+            if j == upIndex:
+                sidefirst = 4
+                sidesnd = 5
+            elif j == downIndex:
+                sidefirst = 6
+                sidesnd = 7
+
+            if flnum == 0:
+                changed.append(j)
+                ns = createCubeFacelet(moveface(facen), sidefacelet(sidefirst), getColor(facelet))
+                pclause = colorEquals(facelet,self.CurrentStateNum, ns, nextStateNum)
+                print pclause
+            elif flnum == 2:
+                changed.append(j)
+                ns = createCubeFacelet(moveface(facen), sidefacelet(sidesnd), getColor(facelet))
+                pclause = colorEquals(facelet,self.CurrentStateNum, ns, nextStateNum)
+                print pclause
+            else:
+                unchanged.append(j)
+        
+        # back (2,3) -> change for 3,1
+        for j in backFacelets:
+            # facelet state
+            facelet = oldState[j]
+            flnum = getFacelet(facelet)
             facen = getFace(facelet)
 
-            # get the state when moved
-            ns = createCubeFacelet(moveface(facen), getFacelet(facelet), getColor(facelet))
-
-            # generate the string
-            pclause = colorEquals(oldState[j],self.CurrentStateNum, ns, nextStateNum)
-            print pclause
+            if flnum == 3:
+                changed.append(j)
+                ns = createCubeFacelet(moveface(facen), sidefacelet(2), getColor(facelet))
+                pclause = colorEquals(facelet,self.CurrentStateNum, ns, nextStateNum)
+                print pclause
+            elif flnum == 1:
+                changed.append(j)
+                ns = createCubeFacelet(moveface(facen), sidefacelet(3), getColor(facelet))
+                pclause = colorEquals(facelet,self.CurrentStateNum, ns, nextStateNum)
+                print pclause
+            else:
+                unchanged.append(j)
 
     def rightMove(self, dir):
         moveface = rightG
         movefacelet = cw
+        sidefacelet = cwright
+
         if dir == CCW:
             moveface = rightGprime
             movefacelet = ccw
+            sidefacelet = ccwright
         elif dir == HALF:
             moveface = rightG2
             movefacelet = half
+            sidefacelet = halfright
 
         nextStateNum = self.CurrentStateNum + 1
         # create new deep copies of states
@@ -362,17 +409,59 @@ class RubiksCube:
             print pclause
         print "-----"
         
-        for j in changed:
+        #changed = []
+        # front (0,1) -> change for 0,2
+        rangefl = frontFacelets + upFacelets + downFacelets
+        sidefirst = 0
+        sidesnd = 1
+        print "length" + str(len(rangefl))
+        for j in rangefl:
             # facelet state
             facelet = oldState[j]
+            flnum = getFacelet(facelet)
+            facen = getFace(facelet)
+            
+            if j == upIndex:
+                sidefirst = 4
+                sidesnd = 5
+            elif j == downIndex:
+                sidefirst = 6
+                sidesnd = 7
+
+            if flnum == 0:
+                changed.append(j)
+                ns = createCubeFacelet(moveface(facen), sidefacelet(sidefirst), getColor(facelet))
+                pclause = colorEquals(facelet,self.CurrentStateNum, ns, nextStateNum)
+                print pclause
+            elif flnum == 2:
+                changed.append(j)
+                ns = createCubeFacelet(moveface(facen), sidefacelet(sidesnd), getColor(facelet))
+                pclause = colorEquals(facelet,self.CurrentStateNum, ns, nextStateNum)
+                print pclause
+            else:
+                unchanged.append(j)
+
+        # back (2,3) -> change for 3,1
+        for j in backFacelets:
+            # facelet state
+            facelet = oldState[j]
+            flnum = getFacelet(facelet)
             facen = getFace(facelet)
 
-            # get the state when moved
-            ns = createCubeFacelet(moveface(facen), getFacelet(facelet), getColor(facelet))
+            if flnum == 3:
+                changed.append(j)
+                ns = createCubeFacelet(moveface(facen), sidefacelet(2), getColor(facelet))
+                pclause = colorEquals(facelet,self.CurrentStateNum, ns, nextStateNum)
+                print pclause
+            elif flnum == 1:
+                changed.append(j)
+                ns = createCubeFacelet(moveface(facen), sidefacelet(3), getColor(facelet))
+                pclause = colorEquals(facelet,self.CurrentStateNum, ns, nextStateNum)
+                print pclause
+            else:
+                unchanged.append(j)
 
-            # generate the string
-            pclause = colorEquals(oldState[j],self.CurrentStateNum, ns, nextStateNum)
-            print pclause
+# end move functions
 
     #incompleted
     def createMappings(self):
@@ -384,18 +473,44 @@ class RubiksCube:
     def addState(self,state):
         self.States.append(state)
 
+    # does all the moves for a state
+    def allMoves(self):
+        # 18 possible moves, 3 per face(3*6=18)
+        movef = [frontMove, leftMove, backMove, rightMove, upMove,downMove]
+        
+        # all the moves that were done
+        movesdone = []
 
+        # which move determines which face will move
+        whichmove = 0
+        # whichdir is what direction will the face move
+
+        for i in range(1,18):
+            whichdir = i % 3
+            if whichdir == 0:
+                movesdone.append(movef[whichmove](HALF))
+            elif whichdir == 1:
+                movesdone.append(movef[whichmove](CW))
+            elif whichdir == 2:
+                movesdone.append(movef[whichmove](CCW))
+
+            if i % 3 == 0:
+                whichmove = whichmove + 1
+
+        return movesdone
 
 # test code
-colornums = range(1,25)
-pcolornums = range(1,25)
+colornums = range(1,MAXFACELETS+1)
+pcolornums = range(1,MAXFACELETS+1)
 shuffle(pcolornums)
 sample = RubiksCube(colornums)
 states = sample.getStates()
 
-sample.upMove(CCW)
-print "---down---"
-sample.downMove(CW)
+#sample.upMove(CCW)
+#print "---down---"
+#sample.downMove(CW)
+print "---left---"
+sample.leftMove(CW)
 #print states[0]
 #for i in range(0,1):
 #   print colorFunctionCubeState(sample.States[i][0],i)
